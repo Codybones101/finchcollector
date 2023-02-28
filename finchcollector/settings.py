36d1a7ab3318e80c2e8f1a36e4633f132ddc8537
CodyBones101 <iamaplsoz@gmail.com>
2023-02-28 13:37:23 -0500

    3rd commit

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import environ
+environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'finchcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'fincollector',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CodyBones101/finchcollector',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PW'],
+        'HOST': 'db.bit.io',
+        'PORT': '5432', 
     }
 }
 
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 finches = [
-  {'name': 'tommy', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+  {'name': 'tommy', 'breed': 'goldfinch', 'description': 'beautiful yellow coloring', 'age': 2},
+  {'name': 'argile', 'breed': 'crossbill', 'description': 'happy little guy', 'age': 1},
 ]
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
