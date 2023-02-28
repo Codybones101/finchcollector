d3a355b6c4401be12f63cfbb6b3d4b626837f2f1
CodyBones101 <iamaplsoz@gmail.com>
2023-02-28 11:55:30 -0500

    Second Commit

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'tommy', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
 ]
 # Create your views here.
