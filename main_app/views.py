7950f0f5536d22534eedfbf99f08828d8925fb34
CodyBones101 <iamaplsoz@gmail.com>
2023-03-03 00:13:59 -0500

    Add toys

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from .models import Finch
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
-from .forms import FeedingForm
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
 
 
 # Create your views here.
@@ -21,22 +19,20 @@ def finches_index(request):
         'finches': finches
     })
 
-def finches_detail(request, finch_id):
-  finch = Finch.objects.get(id=finch_id)
-  feeding_form = FeedingForm()
-  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
-
-def add_feeding(request, finch_id):
-  # create a ModelForm instance using the data in request.POST
-  form = FeedingForm(request.POST)
-  # validate the form
-  if form.is_valid():
-    # don't save the form to the db until it
-    # has the cat_id assigned
-    new_feeding = form.save(commit=False)
-    new_feeding.finch_id = finch_id
-    new_feeding.save()
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
   return redirect('detail', finch_id=finch_id)
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+    'finch': finch, 'feeding_form': feeding_form,
+    'toys': toys_finch_doesnt_have
+  })
 
 class FinchCreate(CreateView):
   model = Finch
@@ -45,9 +41,39 @@ class FinchCreate(CreateView):
 
 class FinchUpdate(UpdateView):
   model = Finch
-  # Let's disallow the renaming of a cat by excluding the name field!
   fields = ['breed', 'description', 'age']
 
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description', 'age']
+
